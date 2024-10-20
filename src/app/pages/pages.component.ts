c0a04ceec1751d602065ce7713fd1327202dfa9f
Dani Castro <orcacodefree@gmail.com>
2024-10-19 23:03:07 -0600

    Fin seccion 7

---
+++ src/app/pages/pages.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import {SettingsService } from '../services/settings.service';
+declare function customInitElements();
+
+
+@Component({
+  selector: 'app-pages',
+  templateUrl: './pages.component.html',
+  styles: [
+  ]
+})
+export class PagesComponent implements OnInit {
+  public linkTheme = document.querySelector('#theme');
+  constructor(private settingsService: SettingsService) { }
+
+  ngOnInit(): void {
+    customInitElements();
+  }
+
+}
